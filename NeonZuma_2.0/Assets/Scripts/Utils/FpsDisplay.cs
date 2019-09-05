9009b23728b6b3e867a955acb14f31493de27c51
Maksym Marchenko <mamarchenko@amcbridge.com>
2019-09-05 14:49:57 +0300

    Added FPS counter

---
+++ NeonZuma_2.0/Assets/Scripts/Utils/FpsDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class FpsDisplay : MonoBehaviour
+{
+    [SerializeField] private float updateFrecuency = 1f;
+
+    private Text text;
+    private WaitForSeconds waitForDelay;
+
+    private void Start()
+    {
+        text = GetComponent<Text>();
+        StartCoroutine(UpdateCounter());
+    }
+
+    private IEnumerator UpdateCounter()
+    {
+        waitForDelay = new WaitForSeconds(updateFrecuency);
+
+        while (true)
+        {
+            var lastFrameCount = Time.frameCount;
+            var lastTime = Time.realtimeSinceStartup;
+
+            yield return waitForDelay;
+
+            var timeDelta = Time.realtimeSinceStartup - lastTime;
+            var frameDelta = Time.frameCount - lastFrameCount;
+
+            text.text = string.Format("{0:0.} FPS", frameDelta / timeDelta);
+        }
+    }
+}
